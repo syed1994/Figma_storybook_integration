aac139461b9c0c3584710dac2d4255c943ab2e8c
syed salman <syedsalman@syeds-MBP.domain.name>
2023-05-23 19:23:42 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -9,13 +9,13 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
+        <as
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React here
         </a>
       </header>
     </div>
